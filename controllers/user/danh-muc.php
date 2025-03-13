ac6676f5d00df6d47765dbbf6f2aead51b406b1d
subway90 <nguyenhieu3105@gmail.com>
2025-03-13 16:49:44 +0700

    update case danh-muc : query cate v1 & v2

--- controllers/user/danh-muc.php
+++ controllers/user/danh-muc.php
@@ -7,28 +7,31 @@ model('user','category');
 $result = '';
 $slug_request_category_v1 = $slug_request_category_v2 = '';
 $request_brand = $request_color = $request_price = '';
-$query_brand = $query_color = $query_price = '';
+$query_brand = $query_color = $query_price = $query_cate_v2 = 'IS NOT NULL';
 
 # [HANDLE]
 
 // lấy slug danh mục v1
-if(isset($_arrayURL[1])) {
+if(get_action_uri(1)) {
     // lấy slug
-    $slug_request_category_v1 = clear_input($_arrayURL[1]);
+    $slug_request_category_v1 = clear_input(get_action_uri(1));
     // kiểm tra tồn tại
     if(!check_exist_one_by_slug('category_v1',$slug_request_category_v1)) view_error(404);
 }else view_error(404);
 
 // lấy slug danh mục v2
-if(isset($_arrayURL[2])) {
+if(get_action_uri(2)) {
     // lấy slug
-    $slug_request_category_v2 = clear_input($_arrayURL[2]);
+    $slug_request_category_v2 = clear_input(get_action_uri(2));
     // kiểm tra tồn tại
     if(!check_exist_one_by_slug('category_v2',$slug_request_category_v2)) view_error(404);
+    // tạo câu truy vấn
+    $query_cate_v2 = '="'.$slug_request_category_v2.'"';
 }
 
 // lấy danh sách sản phẩm
 if(isset($_GET['filter'])) {
+    
     // lấy điều kiện lọc
     if(isset($_GET['price']) && $_GET['price']) $request_price = clear_input($_GET['price']);
     if(isset($_GET['brand']) && $_GET['brand']) $request_brand = clear_input($_GET['brand']);
@@ -39,8 +42,10 @@ if(isset($_GET['filter'])) {
             if($item['value'] == $request_price) $query_price = $item['query'];
         }
     }
+
     // lấy câu truy vấn lọc theo thương hiệu
     if($request_brand) $query_brand = ' = '.$request_brand;
+
     // lấy câu truy vấn lọc theo màu
     if($request_color) $query_color = ' = '.$request_color;
 
@@ -53,7 +58,11 @@ if(isset($_GET['filter'])) {
         LEFT JOIN color cl ON p.id_color = cl.id_color
         LEFT JOIN product_category pc ON  p.id_product = pc.id_product
         LEFT JOIN product_image pi ON p.id_product = pi.id_product
-        WHERE p.price_product '.$query_price.'
+        LEFT JOIN category_v2 c2 ON pc.id_category_v2 = c2.id_category_v2
+        LEFT JOIN category_v1 c1 ON c2.id_category_v1 = c1.id_category_v1
+        WHERE c1.slug_category_v1 = "'.$slug_request_category_v1.'"
+        AND c2.slug_category_v2 '.$query_cate_v2.'
+        AND p.price_product '.$query_price.'
         AND p.id_brand '.$query_brand.'
         AND p.id_color '.$query_color.'
         GROUP BY p.id_product
