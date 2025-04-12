aa650b2448725a03c7b12698c5457ff3b89d9be4
subway90 <nguyenhieu3105@gmail.com>
2025-04-12 19:33:41 +0700

    update autoload : clean code

--- autoload.php
+++ autoload.php
@@ -5,10 +5,10 @@ session_start();
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 // Khởi tạo các session
-if(!isset($_SESSION['user'])) $_SESSION['user'] = '';
-if(!isset($_SESSION['cart']) || !is_array($_SESSION['cart'])) $_SESSION['cart'] = [];
-if(!isset($_SESSION['toast']) || !is_array($_SESSION['toast'])) $_SESSION['toast'] = [];
-if(!isset($_SESSION['checkout']) || !is_array($_SESSION['checkout'])) $_SESSION['checkout'] = [];
+if(!isset($_SESSION['user'])) $_SESSION['user'] = [];
+if(!isset($_SESSION['cart'])) $_SESSION['cart'] = [];
+if(!isset($_SESSION['toast'])) $_SESSION['toast'] = [];
+if(!isset($_SESSION['checkout'])) $_SESSION['checkout'] = [];
 if(!isset($_SESSION['canvas'])) $_SESSION['canvas'] = '';
 
 // config
