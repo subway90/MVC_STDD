1331d7f2a477a9839523fff9227bfd9ab4067f90
subway90 <nguyenhieu3105@gmail.com>
2025-05-27 20:30:10 +0700

    update case choose-v1 : render option chosen in session temp

--- controllers/admin/choose-v1.php
+++ controllers/admin/choose-v1.php
@@ -3,22 +3,42 @@
 # [MODEL]
 
 # [HANDLE]
-if(isset($_POST['name_v1'])) {
-    $name_v1 = $_POST['name_v1'];
+if(isset($_POST['id_category_v1'])) {
+    $id_category_v1 = $_POST['id_category_v1'];
     $data_return = '';
 
-    if($name_v1 !== 'none'){
+    if($id_category_v1 !== 'none'){
         $data = pdo_query(
-        'SELECT *
+        'SELECT v2.*
             FROM category_v2 v2
             LEFT JOIN category_v1 v1
             ON v2.id_category_v1 = v1.id_category_v1
-            WHERE v1.name_category_v1 = "'.$name_v1.'"'
+            WHERE v1.id_category_v1 = '.$id_category_v1
         );
 
         if(!empty($data)) {
             foreach ($data as $row) {
                 $data_return .= '<option value="'. $row['id_category_v2'] .'">'. $row['name_category_v2'] .'</option>';
+            }
+        }
+    }else {
+        if(!empty($_SESSION['selected_product_category']['v1'])) {
+            $data = pdo_query(
+            'SELECT v2.*
+                FROM category_v2 v2
+                LEFT JOIN category_v1 v1
+                ON v2.id_category_v1 = v1.id_category_v1
+                WHERE v1.id_category_v1 = '. $_SESSION['selected_product_category']['v1']
+            );
+
+            if(!empty($data)) {
+                foreach ($data as $row) {
+                    $selected = false;
+                    foreach($_SESSION['selected_product_category']['v2'] as $id_v2) {
+                        if($id_v2 == $row['id_category_v2']) $selected = 'selected';
+                    }
+                    $data_return .= '<option '.$selected.' value="'. $row['id_category_v2'] .'">'. $row['name_category_v2'] .'</option>';
+                }
             }
         }
     }
