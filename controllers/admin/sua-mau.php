8c2bfba0db3e7c2fc56cb5616e772d09acb4a779
subway90 <nguyenhieu3105@gmail.com>
2025-06-01 09:42:46 +0700

    add case admin/sua-mau

---
+++ controllers/admin/sua-mau.php
@@ -0,0 +1,60 @@
+<?php
+
+# [MODEL]
+model('admin','color');
+
+# [VARIABLE]
+$list_error = [];
+
+# [HANDLE]
+
+// lấy thông tin màu cần sửa
+if(!get_action_uri(2)) view_error(404);
+// lấy id
+$id_color = get_action_uri(2);
+// query
+$get_color = pdo_query_one_new(
+    'SELECT * FROM color
+    WHERE id_color = ?
+    AND deleted_at IS NULL',
+    $id_color
+);
+// validate
+if(!$get_color) view_error(404);
+
+// Gán data
+$name_color = $get_color['name_color'];
+$code_color = $get_color['code_color'];
+
+// Nếu sửa
+if(isset($_POST['edit'])) {
+    // input
+    $name_color = $_POST['name_color'];
+    $code_color = $_POST['code_color'];
+    // validate
+    if(!$name_color) $list_error[] = 'Chưa nhập tên màu';
+    if(!$code_color) $list_error[] = 'Chưa nhập mã màu';
+    else if(check_exist_one_by_name_except_id('color',$name_color,$id_color)) $list_error[] = 'Tên màu này đã tồn tại';
+    // lưu
+    if(empty($list_error)) {
+        // query sql
+        pdo_execute_new(
+            'UPDATE color SET name_color = ?, slug_color = ?, code_color = ?, updated_at = current_timestamp() WHERE id_color = ?',
+            $name_color,create_slug($name_color),$code_color,$id_color
+        );
+        // thông báo
+        toast_create('success','Sửa màu thành công');
+        // chuyển hướng
+        route('admin/danh-sach-mau');
+    }
+}
+
+# [DATA
+$data = [
+    'list_error' => $list_error,
+    'name_color' => $name_color,
+    'code_color' => $code_color,
+];
+
+# [RENDER]
+view('admin','Sửa màu','color-edit',$data);
---
+++ views/admin/color-edit.php
@@ -0,0 +1,63 @@
+<form action="" method="post">
+    <div id="top" class="sa-app__body">
+        <div class="mx-sm-2 px-2 px-sm-3 px-xxl-4 pb-6">
+            <div class="container">
+                <div class="py-5">
+                    <div class="row g-4 align-items-center">
+                        <div class="col">
+                            <h1 class="h3 m-0">Sửa màu</h1>
+                        </div>
+                        <div class="col-auto d-flex">
+                            <a href="<?= URL_ADMIN?>danh-sach-mau" class="btn btn-secondary me-3">Huỷ</a>
+                            <button name="edit" type="submit" class="btn btn-primary">Cập nhật</button>
+                        </div>
+                        
+                        <div class="col-12">
+                            <?= show_error($list_error) ?>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="sa-entity-layout"
+                    data-sa-container-query="{&quot;920&quot;:&quot;sa-entity-layout--size--md&quot;,&quot;1100&quot;:&quot;sa-entity-layout--size--lg&quot;}">
+                    <div class="sa-entity-layout__body">
+                        <div class="sa-entity-layout__main">
+                            <div class="card">
+                                <div class="card-body p-5 row">
+
+                                    <div class="col-12 col-lg-6 mb-4">
+                                        <label for="name_color" class="form-label">Tên màu <span class="text-danger">(✱)</span></label>
+                                        <input type="text" class="form-control" name="name_color" id="name_color" value="<?= $name_color ?>" placeholder="Tên màu" />
+                                    </div>
+
+                                    <div class="col-12 col-lg-6 mb-4">
+                                        <label for="code_color" class="form-label">Mã màu <span class="text-danger">(✱)</span></label>
+                                        <div class="d-flex align-items-center gap-3">
+                                            <input type="color" class="form-control" name="code_color" id="code_color" value="<?= $code_color ?>" placeholder="Mã màu" />
+                                            <span class="text-muted text-uppercase small" id="code_color_show">#000000</span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<script>
+    $(document).ready(function() {
+        // Khởi tạo giá trị span theo mã màu mặc định
+        $('#code_color_show').text($('#code_color').val());
+
+        // Cập nhật span khi input màu thay đổi
+        $('#code_color').on('input', function() {
+            var colorValue = $(this).val();
+            $('#code_color_show').text(colorValue); // Cập nhật mã hex trong span
+        });
+    });
+</script>
