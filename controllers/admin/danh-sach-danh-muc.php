7ff605d13990d54574d6c180c801eb1e1722995b
subway90 <nguyenhieu3105@gmail.com>
2025-06-01 21:16:25 +0700

    update case admin/danh-sach-danh-muc : add feature delete & restore

--- controllers/admin/danh-sach-danh-muc.php
+++ controllers/admin/danh-sach-danh-muc.php
@@ -14,27 +14,27 @@ if(get_action_uri(2) === 'danh-sach-xoa') $status_page = false;
 // Nếu xoá
 if(isset($_POST['delete']) && $_POST['delete']) {
     // input
-    $id_brand = $_POST['delete'];
+    $id_category = $_POST['delete'];
     // validate
-    if(check_exist_one('brand',$id_brand)) {
+    if(check_exist_one('category_v1',$id_category)) {
         // delete
-        delete_one('brand',$id_brand);
+        delete_one('category_v1',$id_category);
         // thông báo
-        toast_create('success','Xoá thành công thương hiệu');
-    }else toast_create('danger','Không tồn tại thương hiệu này');
+        toast_create('success','Xoá thành công danh mục');
+    }else toast_create('danger','Không tồn tại danh mục này');
 }
 
 // Nếu khôi phục
 if(isset($_POST['restore']) && $_POST['restore']) {
     // input
-    $id_brand = $_POST['restore'];
+    $id_category = $_POST['restore'];
     // validate
-    if(check_exist_one_in_trash('brand',$id_brand)){
+    if(check_exist_one_in_trash('category_v1',$id_category)){
         // delete
-        restore_one('brand',$id_brand);
+        restore_one('category_v1',$id_category);
         // thông báo
-        toast_create('success','Khôi phục thành công thương hiệu');
-    } else toast_create('danger','Không tồn tại thương hiệu này ở danh sách xoá');
+        toast_create('success','Khôi phục thành công danh mục');
+    } else toast_create('danger','Không tồn tại danh mục này ở danh sách xoá');
     
 }
 
@@ -44,7 +44,7 @@ if(isset($_POST['restore']) && $_POST['restore']) {
 $data = [
     'status_page' => $status_page,
     'list_category' => get_all_category($status_page),
-    'id_v1_open' => 0,
+    'id_v1_open' => $id_v1_open,
 ];
 
 
