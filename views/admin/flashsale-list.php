93e61dc291d2e44f067d90df504f10dc23becfdf
subway90 <nguyenhieu3105@gmail.com>
2025-06-16 20:40:06 +0700

    update view admin/flashsale-list

--- views/admin/flashsale-list.php
+++ views/admin/flashsale-list.php
@@ -60,11 +60,33 @@
                                 <div class="d-flex align-items-center">
                                     <div class="thumbnail-container d-flex align-items-center">
                                         <img class="thumbnail me-3" height="32" src="<?= URL_STORAGE . $banner_flashsale ?>" alt="<?= $banner_flashsale ?>">
-                                        <span class="text-muted">-</span>
-                                        <div class="hover-text text-light"><i class="bi bi-zoom-in"></i></div>
                                     </div>  
-                                    <div class="ms-3">
-                                        <a class="text-dark" href="<?=URL_ADMIN?>sua-flashsale/<?=$id_flashsale?>"><strong><?= $name_flashsale ?></strong></a>
+                                </div>
+                                
+                                <div class="mt-3 small">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="text-muted w-25">
+                                            Tên sự kiện :
+                                        </div>
+                                        <div class="fw-bold">
+                                            <?= $name_flashsale ?>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="text-muted w-25">
+                                            Bắt đầu :
+                                        </div>
+                                        <div class="fw-bold">
+                                            <?= format_time($time_start_flashsale,'hh:mm DD/MM/YYYY') ?>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-center gap-2">
+                                        <div class="text-muted w-25">
+                                            Kết thúc :
+                                        </div>
+                                        <div class="fw-bold">
+                                            <?= format_time($time_end_flashsale,'hh:mm DD/MM/YYYY') ?>
+                                        </div>
                                     </div>
                                 </div>
                             </td>
