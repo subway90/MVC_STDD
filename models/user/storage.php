161ba18c5fdf82db84fc9825335ed2f3466197b1
subway90 <nguyenhieu3105@gmail.com>
2025-06-18 16:47:47 +0700

    update model storage : access .webp

--- models/user/storage.php
+++ models/user/storage.php
@@ -4,7 +4,15 @@
 function resizeImage($filePath, $newWidth, $newHeight) {
     list($width, $height, $type) = getimagesize($filePath);
     $newImage = imagecreatetruecolor($newWidth, $newHeight);
-    
+
+    // Đặt màu nền trong suốt cho PNG và WEBP
+    if ($type == IMAGETYPE_PNG || $type == IMAGETYPE_WEBP) {
+        imagealphablending($newImage, false);
+        imagesavealpha($newImage, true);
+        $transparent = imagecolorallocatealpha($newImage, 255, 255, 255, 127);
+        imagefilledrectangle($newImage, 0, 0, $newWidth, $newHeight, $transparent);
+    }
+
     switch ($type) {
         case IMAGETYPE_JPEG:
             $source = imagecreatefromjpeg($filePath);
@@ -15,16 +23,26 @@ function resizeImage($filePath, $newWidth, $newHeight) {
         case IMAGETYPE_GIF:
             $source = imagecreatefromgif($filePath);
             break;
+        case IMAGETYPE_WEBP:
+            $source = imagecreatefromwebp($filePath);
+            break;
         default:
             throw new Exception("Unsupported image type.");
     }
 
+    // Resize ảnh
     imagecopyresampled($newImage, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-    
-    // Lưu ảnh đã resize
-    header("Content-Type: image/jpeg");
-    imagejpeg($newImage);
-    
+
+    // Xuất ảnh dựa trên loại định dạng
+    header("Content-Type: image/jpeg"); // Mặc định xuất sang JPEG
+    if ($type == IMAGETYPE_WEBP) {
+        header("Content-Type: image/webp");
+        imagewebp($newImage); // Xuất ảnh WEBP
+    } else {
+        imagejpeg($newImage); // Xuất ảnh JPEG
+    }
+
+    // Giải phóng bộ nhớ
     imagedestroy($source);
     imagedestroy($newImage);
 }
@@ -51,6 +69,9 @@ function original_image($path_file) {
             break;
         case IMAGETYPE_GIF:
             $image = imagecreatefromgif($path_file);
+            break;
+        case IMAGETYPE_WEBP:
+            $image = imagecreatefromwebp($path_file);
             break;
         default:
             die("Unsupported image type.");
